e12bbcbec27e72108cfc10d0ce54f6b961789b7d
Arvind <jsrarvind12@gmail.com>
2026-03-28 23:45:46 +0530

    setting up Schema

--- src/models/tweet.js
+++ src/models/tweet.js
@@ -1,1 +1,15 @@
 const mongoose = require('mongoose');
+
+const tweetSchema = new mongoose.Schema({
+    content:{
+        type:String,
+        required:true
+    },
+    userEmail:{
+        type:String
+    }
+},{timestamps:true});
+
+// as you do timestamp:true there will also be two more key and value 1.createdAt:______ 2.updatedAt:______ in which you will got timings
+const tweet = mongoose.model('Tweet',tweetSchema);
+module.exports = tweet;
--- src/server.js
+++ src/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const connect = require('./config/database.js')
 const App = express();
+
+// after creating models in ./models/Tweet.js now import
+
+const Tweet = require('./models/tweet.js');
+
 App.listen(3000,async()=>{
     console.log("App Started on the Port : ",3000); 
     await connect();
     console.log('DB connected.');
+
+    // here the connect of models
+
+    const tweet = await Tweet.create({
+        content:'First tweet',
+        userEmail:'a@b.com'
+    });
+
+    console.log(tweet);
+
 });  
 
 //const { connect, disconnect } = require('./db'); you can doo this if you are exporting using {curley brace}
