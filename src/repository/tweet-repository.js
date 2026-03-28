17ad6758830dd80405261f399895e8f904d2c2eb
Arvind <jsrarvind12@gmail.com>
2026-03-29 00:28:03 +0530

    Done with repository Layers Means all Logic of DataBase Query

---
+++ src/repository/tweet-repository.js
@@ -0,0 +1,45 @@
+const Tweet = require("../models/tweet");
+// all the bussiness logic of database written in this folder
+// database Query
+class TweetRepository {
+  async create(data) {
+    try {
+      const tweet = await Tweet.create(data);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async get(id) {
+    try {
+      const tweet = await Tweet.findById(id);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  async update(tweetId,data) {
+
+    try {
+                const tweet = await Tweet.findByIdAndUpdate(tweetId,data,{new:true});
+                return tweet
+            } catch (error) {
+                    console.log(error);
+            }
+  }
+
+  async destroy(id) {
+    try {
+      const tweet = await Tweet.findByIdAndRemove(id);
+      return tweet;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+};
+
+module.exports = TweetRepository;
+
+// now Exports From Here and import in server.js
--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const App = express();
 
 const Tweet = require('./models/tweet.js');
 
+// after creating tweet repository
+
+const TweetRepository = require('./repository/tweet-repository.js')
+
 App.listen(3000,async()=>{
     console.log("App Started on the Port : ",3000); 
     await connect();
@@ -25,13 +29,20 @@ App.listen(3000,async()=>{
 
     // -----> we can apply queries on the data base also
     
-    const tweets = await Tweet.find(); // will return array of all documents
-    const tweetId = await Tweet.find({userEmail:'a@b.com'});
-    const tweetParticular = await Tweet.findById('69c81972b966ceeb7bdfa466');  // will give the particular tweets 
-    tweetParticular.userEmail="arvind@21gmail.com"; 
-    await tweetParticular.save(); // like this you can update the document also
-    console.log(tweetParticular); // this will give updatedAt props - means when the document got updated
- 
-});  
+    // const tweets = await Tweet.find(); // will return array of all documents
+    // const tweetId = await Tweet.find({userEmail:'a@b.com'});
+    // const tweetParticular = await Tweet.findById('69c81972b966ceeb7bdfa466');  // will give the particular tweets 
+    // tweetParticular.userEmail="arvind@21gmail.com"; 
+    // await tweetParticular.save(); // like this you can update the document also
+    // console.log(tweetParticular); // this will give updatedAt props - means when the document got updated
 
+
+
+        // now we have made Repository so we no need to do byHand this databaseQuery so comment out all
+
+        const tweetrepo = new TweetRepository();
+        const tweetRepoPrticular = await tweetrepo.get('69c81972b966ceeb7bdfa466');
+        const tweetRepoPrticularAndUpdate = await tweetrepo.update('69c81972b966ceeb7bdfa466',{content:"I am Updating"});
+        console.log(tweetRepoPrticularAndUpdate);
+});  
 //const { connect, disconnect } = require('./db'); you can doo this if you are exporting using {curley brace}
